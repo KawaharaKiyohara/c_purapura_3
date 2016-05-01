3384c245944645b5c88c8e9305b58a3ee315cd7b
TakayukiKiyohara <nezumimusume30@gmail.com>
2016-05-01 16:55:39 +0900

    パックマンコンソールの更新。

--- Lesson1/PackmanConsol/PackmanConsol/PackmanConsol.cpp
+++ Lesson1/PackmanConsol/PackmanConsol/PackmanConsol.cpp
@@ -29,6 +29,9 @@ static int sMap[MAP_HEIGHT][MAP_WIDTH] = {
 	{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
 };
 
+//フレームバッファ・・・のようなもの。
+static char sFrameBuffer[MAP_HEIGHT][MAP_WIDTH];
+
 void EndFrame()
 {
 	Sleep(100);	//100ミリ秒眠る。
@@ -39,6 +42,7 @@ void EndFrame()
 
 	SetConsoleCursorPosition(hCons, pos);
 }
+
 /*!
  *@brief	プレイヤークラス。
  */
@@ -62,6 +66,14 @@ public:
 	~CPlayer()
 	{
 
+	}
+	/*!
+	*@brief	プレイヤーの描画処理。
+	*/
+	void Draw()
+	{
+		//フレームバッファにドロー。
+		sFrameBuffer[m_posY][m_posX] = 'P';
 	}
 	/*!
 	 *@brief	移動処理。
@@ -118,29 +130,137 @@ public:
 		m_posY = y;
 	}
 };
+CPlayer g_player;
+
+/*!
+*@brief	食べ物クラス。
+*/
+class CFood {
+private:
+	int m_posX;		//!<x座標。
+	int m_posY;		//!<y座標
+public:
+	/*!
+	*@brief	コンストラクタ。
+	*/
+	CFood()
+	{
+		m_posX = 0;
+		m_posY = 0;
+	}
+	/*!
+	*@brief	壁の描画処理。
+	*/
+	void Draw()
+	{
+		//フレームバッファにドロー。
+		sFrameBuffer[m_posY][m_posX] = 'F';
+	}
+	/*!
+	*@brief	座標を設定。
+	*/
+	void SetPosition(int x, int y)
+	{
+		m_posX = x;
+		m_posY = y;
+	}
+	/*!
+	*@brief	更新処理。
+	*/
+	void Update()
+	{
+		//プレイヤーと触れたら食べ物が消えるようにしてみよう・・・
+		//プレイヤーはg_playerでアクセスできる。
+		//プレイヤーとCFoodの座標が同じになった時に、消えるようにすればいいはず。
+		//プレイヤーの座標はg_player.GetPositionX()とg_player.GetPositionY()で取得できる。
+		//例えばプレイヤーのX座標が欲しいならこんな感じ。
+		//int pos_x = g_player.GetPositionX();
+	}
+};
+/*!
+*@brief	壁クラス。
+*/
+class CWall {
+private:
+	int	m_posX;		//!<x座標。
+	int m_posY;		//!<y座標
+public:
+	/*!
+	*@brief	コンストラクタ。
+	*/
+	CWall()
+	{
+		m_posX = 0;
+		m_posY = 0;
+	}
+	/*!
+	*@brief	壁の描画処理。
+	*/
+	void Draw()
+	{
+		//フレームバッファにドロー。
+		sFrameBuffer[m_posY][m_posX] = 'W';
+	}
+	/*!
+	*@brief	座標を設定。
+	*/
+	void SetPosition(int x, int y)
+	{
+		m_posX = x;
+		m_posY = y;
+	}
+};
+
 /*!
  *@brief	メイン関数。
  */
 int main()
 {
-	CPlayer player;
 	//プレイヤーの初期位置を決定。
-	player.SetPosition(7, 7);
+	
+	g_player.SetPosition(7, 7);
+	//壁
+	int numWall = 0;	//壁の数。
+	CWall walls[MAP_HEIGHT*MAP_WIDTH];
+	//食べ物
+	int numFood = 0;
+	CFood foods[MAP_HEIGHT*MAP_WIDTH];
+	for (int i = 0; i < MAP_HEIGHT; i++) {
+		for (int j = 0; j < MAP_WIDTH; j++) {
+			if (sMap[i][j] == 1) {
+				//壁。
+				walls[numWall].SetPosition(j, i);
+				//壁を一つ作ったのでカウントアップ。
+				numWall++;
+			}
+			else if (sMap[i][j] == 0) {
+				//食べ物
+				foods[numFood].SetPosition(j, i);
+				//食べ物を一つ作ったのでカウントアップ。
+				numFood++;
+			}
+		}
+	}
 	//ゲームループ。
 	while (true) {
+		//フレームバッファをクリア。
+		memset(sFrameBuffer, 0, sizeof(sFrameBuffer));
 		//プレイヤーの移動処理。
-		player.Move();	
-		//描画。
+		g_player.Move();
+		//食べ物の描画処理。
+		for (int i = 0; i < numFood; i++) {
+			foods[i].Draw();
+		}
+		//壁の描画処理。
+		for (int i = 0; i < numWall; i++) {
+			walls[i].Draw();
+		}
+		//プレーヤーの描画処理。
+		player.Draw();
+		//フレームバッファの内容を画面に表示する。
 		for (int i = 0; i < MAP_HEIGHT; i++) {
 			for (int j = 0; j < MAP_WIDTH; j++) {
-				if (player.GetPositionX() == j && player.GetPositionY() == i) {
-					std::cout << "Ｐ";
-				}else if (sMap[i][j] == 1) {
-					std::cout << "■";
-				}
-				else if (sMap[i][j] == 0) {
-					std::cout << "〇";
-				}
+				std::cout << sFrameBuffer[i][j];
 			}
 			std::cout << "\n";
 		}
